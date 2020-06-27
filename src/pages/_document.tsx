0a02ee54ad066c80621658c46af04dafae509a27
Iyan Saputra <ikhyan7@gmail.com>
2020-06-27 16:04:09 +0800

    seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
 						property='og:description'
 						content='A Fullstack Developer who passionate about modern mobile and web technology using Node JS, React JS, React Native, and Flutter. And would be a fast learner to doing new things and build good teamwork either. Can become a project leader/manager and using tools like Trello to manage project and task.'
 					/>
-					<meta property='og:image' content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png' />
+					<meta property='og:image' content='https://avatars3.githubusercontent.com/u/42711013?s=460&v=4' />
 
 					<meta property='twitter:card' content='summary_large_image' />
 					<meta property='twitter:url' content='https://iyansr.id/' />
@@ -32,10 +32,7 @@ class MyDocument extends Document {
 						property='twitter:description'
 						content='A Fullstack Developer who passionate about modern mobile and web technology using Node JS, React JS, React Native, and Flutter. And would be a fast learner to doing new things and build good teamwork either. Can become a project leader/manager and using tools like Trello to manage project and task.'
 					/>
-					<meta
-						property='twitter:image'
-						content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
-					/>
+					<meta property='twitter:image' content='https://avatars3.githubusercontent.com/u/42711013?s=460&v=4' />
 				</Head>
 				<body>
 					<Main />
