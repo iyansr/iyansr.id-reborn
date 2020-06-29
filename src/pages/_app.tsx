daffa808884193a208c17722ec2bef85414b049d
Iyan Saputra <ikhyan7@gmail.com>
2020-06-29 13:44:52 +0800

    update spinner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import { AppProps } from 'next/app'
 import '../styles/main.scss'
-import NextNprogress from 'nextjs-progressbar'
-import { Fragment } from 'react'
 import 'react-flexbox-grid/dist/react-flexbox-grid.css'
 import 'highlight.js/styles/atom-one-dark-reasonable.css'
 
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-	return (
-		<Fragment>
-			<NextNprogress color='#df49a6' startPosition='0.3' stopDelayMs='200' height='3' />
-			<Component {...pageProps} />
-		</Fragment>
-	)
+	return <Component {...pageProps} />
 }
 
 export default MyApp
