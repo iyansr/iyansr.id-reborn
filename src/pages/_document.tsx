2358db3db173b6d62e85a26f2dd017760fc988b2
Iyan Saputra <ikhyan7@gmail.com>
2020-06-30 13:43:14 +0800

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang='id, in'>
 				<Head>
 					<script async src='https://www.googletagmanager.com/gtag/js?id=UA-171266287-1'></script>
 					<script
