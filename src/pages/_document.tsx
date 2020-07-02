65f79f94dc71ed12b9d11f70699e2e6118eb9d9b
Iyan Saputra <ikhyan7@gmail.com>
2020-07-02 23:59:04 +0800

    sitemap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ class MyDocument extends Document {
 		return (
 			<Html lang='id, in'>
 				<Head>
-					<script data-ad-client='ca-pub-6171352055782473' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
 					<script async src='https://www.googletagmanager.com/gtag/js?id=UA-171266287-1'></script>
 				</Head>
 				<body>
