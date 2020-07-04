c0a5e021585f9ec25f1ece503600b29e0f40a111
Iyan Saputra <ikhyan7@gmail.com>
2020-07-04 10:13:54 +0800

    anayltic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Fragment } from 'react'
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang='id, in'>
-				<Head>{process.env.NODE_ENV === 'production' && <script async src='https://www.googletagmanager.com/gtag/js?id=UA-171266287-1'></script>}</Head>
+				<Head>
+					{process.env.NODE_ENV === 'production' && (
+						<Fragment>
+							<script async src='https://www.googletagmanager.com/gtag/js?id=UA-171266287-1'></script>
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-171266287-1');`,
+								}}></script>
+						</Fragment>
+					)}
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
