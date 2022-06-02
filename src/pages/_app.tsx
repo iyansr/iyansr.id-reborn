adda3519db0cca777f7d0f0f362f57155ff7c32d
IyanSR <ikhyan7@gmail.com>
2022-06-02 14:58:10 +0800

    remove google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
 import { useState } from 'react'
-import * as gtag from '@utils/gtag'
 
 import '@styles/tailwind.scss'
 import '@styles/app.scss'
@@ -16,7 +15,6 @@ import 'nprogress/nprogress.css' //styles of nprogress
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', (url) => {
 	NProgress.done()
-	process.env.NODE_ENV === 'production' && gtag.pageview(url)
 })
 Router.events.on('routeChangeError', () => NProgress.done())
 
