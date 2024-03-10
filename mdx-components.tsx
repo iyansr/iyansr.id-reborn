89f7d5337b7b39c8083df20147b78f411e12e269
I Putu Saputrayana <ikhyan7@gmail.com>
2024-03-10 20:53:01 +0800

    fix buiild

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,6 @@
-import { type MDXComponents } from 'mdx/types'
 import Image, { type ImageProps } from 'next/image'
 
-export function useMDXComponents(components: MDXComponents) {
+export function useMDXComponents(components: any) {
   return {
     ...components,
     Image: (props: ImageProps) => <Image {...props} />,
