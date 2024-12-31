ede88352adf5615ac68e204704baffde407bfce1
I Putu Saputrayana <ikhyan7@gmail.com>
2024-12-31 20:28:33 +0800

    fix metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,6 +7,51 @@ import { ArrowLeft } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+export async function generateStaticParams() {
+  const posts = getBlogPosts()
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+export function generateMetadata({ params }: { params: { slug: string } }) {
+  const post = getBlogPosts().find((post) => post.slug === params.slug)
+  if (!post) {
+    return {}
+  }
+
+  const { title, publishedAt: publishedTime, summary: description, image } = post.metadata
+  const ogImage = image
+    ? image.startsWith('http')
+      ? image
+      : `${baseUrl}${image}`
+    : `${baseUrl}/og?title=${encodeURIComponent(title)}`
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      publishedTime,
+      url: `${baseUrl}/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [ogImage],
+    },
+  }
+}
+
 export default async function Blog({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug
 
